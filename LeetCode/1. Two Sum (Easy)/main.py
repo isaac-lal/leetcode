e266dfba8930e5346da28c6d1f2df4764f38c90b
Isaac Lal <isaaclal124@gmail.com>
2023-08-23 16:43:27 -0400

    Two Sum solved

---
+++ LeetCode/1. Two Sum (Easy)/main.py
@@ -0,0 +1,15 @@
+class Solution(object):
+  def twoSum(self, nums, target):
+    prevMap = {} # val: index
+      
+    for i, n in enumerate(nums):
+      diff = target - n
+      
+      if diff in prevMap:
+        return [prevMap[diff], i]
+      
+      prevMap[n] = i;
+    return
+    
+# Time Complexity: O(n)
+# Memory Space: O(n)
